97569b75ea4502c8cea082821f4fdd7fe25b779d
Kai <kai@mynetmapper.org>
2022-06-10 09:59:32 +0200

    refactor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 
 setup(
     name="qurator-sbb-tools",
-    version="0.0.3",
+    version="0.0.4",
     author="Kai Labusch, The Qurator Team",
     author_email="Kai.Labusch@sbb.spk-berlin.de",
     description="Qurator",
@@ -27,33 +27,11 @@ setup(
         "select-by-lang-and-entropy=qurator.sbb.select:by_lang_and_entropy",
         "select-by-lang=qurator.sbb.select:by_lang",
 
-        "extract-wiki-full-text-sqlite=qurator.wikipedia.xml:to_sqlite",
-        "extract-wiki-ner-entities=qurator.wikipedia.entities:extract",
-        "wikidatamapping=qurator.wikipedia.entities:wikidatamapping",
-        "redirects2entities=qurator.wikipedia.entities:redirects2entities",
-        "redirects2pkl=qurator.wikipedia.entities:redirects2pkl",
-        "compute-apriori-probs=qurator.wikipedia.entities:compute_apriori_probs",
-        "tag-wiki-entities2sqlite=qurator.wikipedia.ner:tag_entities2sqlite",
-        "train-test-split-wiki=qurator.wikipedia.ner:train_test_split",
-
-        "find-entities=qurator.utils.entities:find_entities",
-        "csv2sqlite=qurator.utils.csv:to_sqlite",
-        "df2sqlite=qurator.utils.pickle:to_sqlite",
-        "df-concatenate=qurator.utils.pickle:concatenate",
         "batchner=qurator.sbb.ner:on_db_file",
         "ned-statistics=qurator.sbb.ned:ned_statistics",
         "show-ner-models=qurator.sbb.ner:show_models",
 
-        "run-sparql=qurator.wikidata.cli:cli_run_sparql",
-        "join-entities=qurator.wikidata.cli:join_entities",
-        "batchel=qurator.sbb.ned:run_on_corpus",
-        "batchel-wp=qurator.wikipedia.ned:run_on_tagged",
-
-        "extract-corpus=qurator.topic_modeling.cli:extract_corpus",
-        "extract-docs=qurator.topic_modeling.cli:extract_docs",
-        "lda-grid-search=qurator.topic_modeling.cli:lda_grid_search",
-        "run-lda=qurator.topic_modeling.cli:run_lda",
-        "make-topicm-config=qurator.topic_modeling.cli:make_config"
+        "batchel=qurator.sbb.ned:run_on_corpus"
       ]
     },
     python_requires='>=3.6.0',
